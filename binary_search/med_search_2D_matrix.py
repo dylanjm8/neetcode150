f17b5b299b017adb6be444c8f6cd588ae5bd5729
dylanjm8 <99040755+dylanjm8@users.noreply.github.com>
2025-05-28 21:03:36 -0400

    Update med_search_2D_matrix.py

--- binary_search/med_search_2D_matrix.py
+++ binary_search/med_search_2D_matrix.py
@@ -5,7 +5,7 @@ class Solution:
         top, bot = 0, (rows - 1)
 
         while top <= bot:
-            row = top + bot // 2 
+            row = (top + bot) // 2 
             if (matrix[row][0] > target):
                 bot = (row - 1)
             elif (matrix[row][-1] < target):
@@ -16,14 +16,14 @@ class Solution:
         if not (top <= bot):
             return False
 
-        row = top + bot // 2 # found row
+        row = (top + bot) // 2 # found row
 
         # now we search horizontally
         
         l , r  = 0, cols-1
 
         while l <= r:
-            mid = r + l // 2
+            mid = (r + l) // 2
 
             if (matrix[row][mid] < target):
                 l = mid + 1
