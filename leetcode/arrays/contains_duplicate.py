a933a84696ff51d92968afb4346eddc1238591f2
dylanjm8 <99040755+dylanjm8@users.noreply.github.com>
2025-06-09 17:41:48 -0400

    Create contains_duplicate.py

---
+++ leetcode/arrays/contains_duplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # hashset stores unique elements, dict(hashmap) stores key-value pairs 
+        
+        hs = set() # or {} but the curly braces must be filled to be a hs, or else its a dict (hm)
+        
+        if not nums:
+            return True
+        
+        for n in nums: # not an index, but the actual value
+            if n in hs:
+                return True
+            hs.add(n)
+        return False
